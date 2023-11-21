4ef0ae614d193e21cc5a3fe4c4cd9ada168311be
leekwonho <yaaa3717@naver.com>
2023-11-21 17:47:23 +0900

    cc

--- SOLOPLAY/src/main/java/servlet/MainServlet.java
+++ SOLOPLAY/src/main/java/servlet/MainServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import index.cotroller.IndexController;
 import starbucks.controller.StarbucksController;
 
 public class MainServlet extends HttpServlet {
@@ -18,7 +19,10 @@ public class MainServlet extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		System.out.println(req.getRequestURL());
 		String uri = req.getRequestURI();
-		if (uri.startsWith("/starbucks/")) {
+		if (-1 < uri.indexOf("/index.star")) {
+			IndexController.service(req, res);
+		}
+		if (-1 < uri.indexOf("/starbucks/")) {
 			StarbucksController.service(req, res);
 		}
 
