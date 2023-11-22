aaf30c15bef814e4441ce852edc7e743023c0383
leekwonho <yaaa3717@naver.com>
2023-11-22 14:35:16 +0900

    dd

--- SOLOPLAY/src/main/java/cookie/CookieMaker.java
+++ SOLOPLAY/src/main/java/cookie/CookieMaker.java
@@ -4,17 +4,16 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 public class CookieMaker {
-	
+
 	public static void makeCookie(HttpServletResponse res) {
 		Cookie cookie = new Cookie("BlackFridayGift", "ChocoCookie");
 		cookie.setMaxAge(60 * 60 * 24);
 		res.addCookie(cookie);
 	}
-	
+
 	public static void removeCookie(HttpServletResponse res) {
 		Cookie cookie = new Cookie("BlackFridayGift", "ChocoCookie");
 		cookie.setMaxAge(0);
 		res.addCookie(cookie);
 	}
-
 }
