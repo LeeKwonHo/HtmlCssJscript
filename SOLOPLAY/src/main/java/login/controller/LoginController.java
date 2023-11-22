9b65d42ac801296dfd49e4dda9a67d3f8b61a364
leekwonho <yaaa3717@naver.com>
2023-11-22 13:50:43 +0900

    coooooooooookie

--- SOLOPLAY/src/main/java/login/controller/LoginController.java
+++ SOLOPLAY/src/main/java/login/controller/LoginController.java
@@ -6,18 +6,20 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import index.model.IndexModel;
 import login.model.LoginModel;
+import starbucks.model.StarbucksModel;
 
 public class LoginController {
 
 	public static void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		String uri = req.getRequestURI();
 		if ( -1 < uri.indexOf("/login.star") ) {
-			LoginModel.login(req);
+			LoginModel.login(req, res);
 			req.getRequestDispatcher("/jsp/index.jsp").forward(req, res);
 		}
 		if ( -1 < uri.indexOf("/logout.star") ) {
-			LoginModel.logout(req);
+			LoginModel.logout(req, res);
 			req.getRequestDispatcher("/jsp/index.jsp").forward(req, res);
 		}
 	}
