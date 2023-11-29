e0f1fc9e7594b0aa37771cb8bdb5cdc68fdfcbe9
leekwonho <yaaa3717@naver.com>
2023-11-29 17:09:51 +0900

    YEAH

--- SOLOPLAY/src/main/java/login/model/LoginModel.java
+++ SOLOPLAY/src/main/java/login/model/LoginModel.java
@@ -16,55 +16,45 @@ import cookie.CookieMaker;
 
 public class LoginModel {
 
-	// 폼 로그인
+	//폼 로그인
 	public static void login(HttpServletRequest req, HttpServletResponse res) {
 		String userId = req.getParameter("userId");
 		String userPw = req.getParameter("userPw");
-		if ("hongkd".equals(userId) && "1234".equals(userPw)) {
-			req.getSession().setAttribute("userName", "홍길동");
+		if ( "hongkd".equals(userId) && "1234".equals(userPw) ) {
+			req.getSession().setAttribute("userName","홍길동");
 			CookieMaker.makeCookie(res);
 		} else {
 			req.setAttribute("msg", "아이디 또는 비밀번호를 다시 확인해주세요.");
 		}
 	}
-
-	// ajax 로그인
-	public static String loginA(HttpServletRequest req, HttpServletResponse res)
-			throws JsonSyntaxException, JsonIOException, IOException {
-
+	
+	//ajax 로그인
+	public static String loginA(HttpServletRequest req, HttpServletResponse res) throws JsonSyntaxException, JsonIOException, IOException {
 		JsonObject returnJson = null;
 		JsonObject json = new Gson().fromJson(req.getReader(), JsonObject.class);
 		System.out.println(json);
-
-		JsonObject paramJson = (JsonObject) json.get("param");
-
+		JsonObject paramJson= (JsonObject)json.get("param");
 		String userId = paramJson.get("userId").getAsString();
 		String userPw = paramJson.get("userPw").getAsString();
-
-		if ("hongkd".equals(userId) && "1234".equals(userPw)) {
-
+		if ( "hongkd".equals(userId) && "1234".equals(userPw) ) {
 			req.getSession().setAttribute("userName", userId);
 			CookieMaker.makeCookie(res);
-
-			returnJson = new JsonObject();
-			JsonObject jo = new JsonObject();
 			
+			returnJson = new JsonObject();
 			returnJson.addProperty("resultCode", "1");
+			JsonObject jo = new JsonObject();
 			jo.addProperty("userName", userId);
 			returnJson.add("data", jo);
-			
 		} else {
-			
 			returnJson = new JsonObject();
 			returnJson.addProperty("resultCode", "0");
 		}
-
 		return new Gson().toJson(returnJson);
 	}
-
+	
 	public static void logout(HttpServletRequest req, HttpServletResponse res) {
 		req.getSession().removeAttribute("userName");
 		CookieMaker.removeCookie(res);
 	}
-
+	
 }
